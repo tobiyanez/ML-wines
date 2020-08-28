23e02fefb74062d2a04bf41031203e40dbba7527
tobiyanez <70412645+tobiyanez@users.noreply.github.com>
2020-08-28 17:19:49 -0400

    Add files via upload

---
+++ Coding Sample R ML.R
@@ -0,0 +1,218 @@
+library(tree) #to fit trees
+library(randomForest) #for random forests (and bagging)
+library(gbm) #for boosting
+library(ISLR)
+library(ggplot2)
+library(ROCR)
+library(boot) #for cv.glm function
+library(MASS) #for lda function
+library(ipred)
+
+red <- read.csv("wineQualityReds.csv",header=T)
+white <- read.csv("wineQualityWhites.csv",header=T)
+
+red$type <- c("Red")
+white$type <- c("White")
+alldata <- rbind(red,white)
+alldata$type <- ifelse(alldata$type=="White", 1,0)
+head(alldata) # quick look at total data set
+
+#seeing what pairs look to be generally correlated
+pairs(alldata[,2:13], lower.panel = NULL, main="Scatterplot of Quantitative Variables")
+round(cor(alldata[,2:13]),3)
+#density, sugar, and alcohol all correlated
+#makes sense chemically
+
+
+#establishing quality variable to measure if wine is above or below median quality
+test <- function(x) {
+  ifelse(
+    x<median(alldata$quality),
+    0,
+    1
+  )}
+alldata$quality_cat <- factor(test(alldata$quality))
+data<-alldata
+
+#creating a training data set and a testing data set to determine
+#how well the model works
+RNGkind(sample.kind = "Rejection")
+set.seed(1)
+sample.data<-sample.int(nrow(data), floor(.50*nrow(data)), replace = F)
+train<-data[sample.data, ]
+test<-data[-sample.data, ]
+
+#################################################
+## Regression ##
+#################################################
+
+#setting the response variable to be the alcohol content in the wine
+test.y<-test[,"alcohol"]
+
+#building a simple classification tree 
+tree.reg.train<-tree(alcohol~.-quality_cat, data=train) # using all vars except quality_cat 
+summary(tree.reg.train)
+plot(tree.reg.train)
+text(tree.reg.train, cex=0.75, pretty=0)
+#density is the "most important" factor, makes up the first two splits
+
+tree.pred.test<-predict(tree.reg.train, newdata=test) 
+mean((tree.pred.test - test.y)^2) # test error rate
+
+#used this model on the test data set
+#the test error rate was ~.511 which is not great
+#Lets see if we can improve it
+
+#cross validation regression
+#using K=10 fold cross validation and pruining
+#in order to improve the regression
+cv.reg<-cv.tree(tree.reg.train, K=10)
+cv.reg
+
+trees.num.class<-cv.reg$size[which.min(cv.reg$dev)]
+trees.num.class
+
+plot(cv.reg$size, cv.reg$dev, type='b')
+
+prune.reg<-prune.tree(tree.reg.train, best=trees.num.class)
+prune.reg
+plot(prune.reg)
+text(prune.reg, cex=0.75, pretty=0)
+
+#prediction based on pruned tree for test data
+tree.pred.prune<-predict(prune.reg, newdata=test)
+
+#overall accuracy
+mean((tree.pred.prune - test.y)^2) 
+
+#pruning and cross validation did not impact 
+#test error rate, not a super uncommon occurance
+
+#################################################
+## Bagging ##
+#################################################
+
+#bagging is special case of random forest when mtry = number of predictors
+names(train)
+bag.reg<-randomForest(alcohol~.-quality_cat, data=train, mtry=12, importance=TRUE)
+
+#importance measures of predictors
+importance(bag.reg)
+#graphical version
+varImpPlot(bag.reg)
+
+#the biggest factors in alcohol concentration in wine are the 
+#density and residual sugar content of the wine
+
+pred.bag<-predict(bag.reg, newdata=test)
+mean((pred.bag - test.y)^2)
+# The error rate is down to .180. Much improved!
+
+#################################################
+## Random Forest ##
+#################################################
+
+# Since this is a regression tree p/3 is the sample of predictors each time, which is 4 in this case.
+rf.reg<-randomForest(alcohol~.-quality_cat, data=train, mtry=4, importance=TRUE)
+
+#importance measures of predictors
+importance(rf.reg)
+#graphical version
+varImpPlot(rf.reg)
+
+#density and residual sugar are again shown to be 
+#the two most important variables when predicting alcohol
+#content
+
+#test accuracy with random forest
+pred.rf<-predict(rf.reg, newdata=test)
+mean((pred.rf - test.y)^2)
+#The error rate is .212, much better than normal regression
+#Not quite as low as bagging
+
+#################################################
+## Classification ##
+#################################################
+
+#this time predicting for the quality of the wine
+#strating off again with a simple regression and classification tree
+test.y <- test[,"quality_cat"]
+
+tree.class.train<-tree(quality_cat~.-quality, data=train)
+summary(tree.class.train)
+
+plot(tree.class.train)
+text(tree.class.train, cex=0.75, pretty=0,main="Figure 4.1.4")
+
+tree.pred.test<-predict(tree.class.train, newdata=test, type="class") 
+#confusion matrix for test data
+table(test.y, tree.pred.test)
+mean(tree.pred.test != test.y)
+# test mse of .274 for basic tree
+
+
+#using cross validation and pruning to attempt to improve the tree
+cv.class<-cv.tree(tree.class.train, K=10, FUN = prune.misclass)
+cv.class
+
+trees.num.class<-cv.class$size[which.min(cv.class$dev)]
+trees.num.class
+
+plot(cv.class$size, cv.class$dev, type='b',main="Figure 4.2.1",ylab='Deviance',xlab="Size")
+
+prune.class <-prune.tree(tree.class.train, best=3)
+summary(prune.class)
+plot(prune.class)
+text(prune.class, cex=0.75, pretty=0)
+
+#prediction based on pruned tree for test data
+tree.pred.prune<-predict(prune.class, newdata=test, type="class")
+
+#overall accuracy
+mean(tree.pred.prune != test.y) 
+# test mse of .274, again pruning/cv has no effect on our tree
+
+#################################################
+## Bagging ##
+#################################################
+
+names(train)
+bag.class<-randomForest(quality_cat~.-quality, data=train, mtry=12, importance=TRUE)
+
+#importance measures of predictors
+importance(bag.class)
+#graphical version
+varImpPlot(bag.class)
+
+#alcohol content is by far the biggest predictor in what 
+#makes a wine classify as above average
+
+#test accuracy with bagging
+pred.bag<-predict(bag.class, newdata=test, type = "response")
+
+# Confusion Matrix
+table(test.y, pred.bag)
+mean(pred.bag != test.y)
+#mse down to .196, again much improved!
+
+#################################################
+## Random Forest ##
+#################################################
+
+#Since this is a classification tree we will 
+#use sqrt(p) as our sample of predictors each time, 
+#which is 3 (3.46) in this case.
+rf.class<-randomForest(quality_cat~.-quality, data=train, mtry=3, importance=TRUE)
+
+#importance measures of predictors
+importance(rf.class)
+#graphical version
+varImpPlot(rf.class)
+
+#test accuracy with random forest
+pred.rf<-predict(rf.class, newdata=test, type = "response")
+
+# Confusion Matrix
+table(test.y, pred.rf)
+mean(pred.rf != test.y)
+#.189 mse, even lower than bagging!
